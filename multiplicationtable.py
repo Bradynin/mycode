4199fa4d80f72edfc414d4ca428083fd5446e4a0
Bradynin <104726793+Bradynin@users.noreply.github.com>
2022-10-08 17:18:03 -0400

    Add files via upload
    Multiplication table implementation

---
+++ multiplicationtable.py
@@ -0,0 +1,11 @@
+print(" ", end=" ")
+for j in range(1,10):
+    print(str(j), end = " ")
+print("\n")
+for i in range(1,10):
+    print(str(i), end=" ")
+    for j in range(1,10):
+        print(str(j*i), end = " ")
+    print("\n")
+        
+    
